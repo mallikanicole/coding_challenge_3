e806355a18b7b91347744cd52d9c70f7a0c3d84c
mallikanicole <malmichael23@gmail.com>
2025-01-30 17:46:19 -0500

    task1 test

---
+++ cc_3.js
@@ -0,0 +1,5 @@
+///Task 1: Product Price Management
+let arrayPrices=[24,76,33,68,32]//setting
+console.log("Prices:",arrayPrices)
+arrayPrices.push(81);
+console.log("With Added Price:",arrayPrices)
