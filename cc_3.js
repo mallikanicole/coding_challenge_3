a3603affbe63d382d9ea3519e33b00eb311db924
mallikanicole <malmichael23@gmail.com>
2025-01-30 18:17:42 -0500

    Task 1-Product Price Management

--- cc_3.js
+++ cc_3.js
@@ -1,8 +1,8 @@
 ///Task 1: Product Price Management
-let arrayPrices=[24,76,33,68,32]//setting
+let arrayPrices=[24,76,33,68,32]//setting prices
 console.log("Prices:",arrayPrices)
-arrayPrices.push(81);
+arrayPrices.push(81);//adding new price
 console.log("With Added Price:",arrayPrices)
-let removedItem=arrayPrices.shift()
+let removedItem=arrayPrices.shift();//removing first price
 console.log("Removed Price:",removedItem);
 console.log("Updated Prices:",arrayPrices)
