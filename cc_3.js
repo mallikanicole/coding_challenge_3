07e58931494e2353f41b55205be355d8a9767bc1
mallikanicole <malmichael23@gmail.com>
2025-01-30 18:15:57 -0500

    test task 1 remove price

--- cc_3.js
+++ cc_3.js
@@ -3,4 +3,6 @@ let arrayPrices=[24,76,33,68,32]//setting
 console.log("Prices:",arrayPrices)
 arrayPrices.push(81);
 console.log("With Added Price:",arrayPrices)
-arrayPrices[0]="99";
+let removedItem=arrayPrices.shift()
+console.log("Removed Price:",removedItem);
+console.log("Updated Prices:",arrayPrices)
