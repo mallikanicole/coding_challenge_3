0ccaed2a8bd8625cb3f4d1e465585a521f6f514e
mallikanicole <malmichael23@gmail.com>
2025-01-30 19:33:20 -0500

    Task 2-Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -9,7 +9,10 @@ console.log("Updated Prices:",Prices)
 //Task 2: Modifying Customer Orders
 let Orders=[101,102,103,104,105];
 console.log("Orders:",Orders)
-Orders[2]=103+5
+Orders[2]=103+5;//adding 5 to order 3
 console.log("Orders Updated:",Orders)
+let totalOrders=Orders.reduce((sum,amount)=>sum+amount,0);//sum of orders
+console.log("Total Orders:",totalOrders)
+
 
 
