2ce52e5cc724a220d1e41179e6d177a78c13e685
mallikanicole <malmichael23@gmail.com>
2025-01-30 19:01:51 -0500

    test t2 c

--- cc_3.js
+++ cc_3.js
@@ -1,5 +1,5 @@
 ///Task 1: Product Price Management
-let Prices=[24,76,33,68,32]//setting prices
+let Prices=[24,76,33,68,32];//setting prices
 console.log("Prices:",Prices)
 Prices.push(81);//adding new price
 console.log("With Added Price:",Prices)
@@ -7,9 +7,9 @@ let removedItem=Prices.shift();//removing first price
 console.log("Removed Price:",removedItem);
 console.log("Updated Prices:",Prices)
 //Task 2: Modifying Customer Orders
-let Orders=[101,102,103,104,105]
+let Orders=[101,102,103,104,105];
 console.log("Orders:",Orders)
-Orders[2]=Orders[2]+5
+Orders[2]=103+5
 console.log("Orders Updated:",Orders)
 
 
