3fe9c9ff16ee2056fc3500b81e907f1ada3a58e5
mallikanicole <malmichael23@gmail.com>
2025-01-31 13:36:42 -0500

    added space for readability

--- cc_3.js
+++ cc_3.js
@@ -6,6 +6,8 @@ console.log("With Added Price:",Prices)
 let removedItem=Prices.shift();//removing first price
 console.log("Removed Price:",removedItem);
 console.log("Updated Prices:",Prices)
+
+
 //Task 2: Modifying Customer Orders
 let Orders=[101,102,103,104,105];
 console.log("Orders:",Orders)
@@ -13,6 +15,8 @@ Orders[2]=103+5;//adding 5 to order 3
 console.log("Orders Updated:",Orders)
 let totalOrders=Orders.reduce((sum,amount)=>sum+amount,0);//sum of orders
 console.log("Total Orders:",totalOrders)
+
+
 //Task 3: Employee Performance Tracking Scenario
 let employee= {
     name:"Lilac Silver",
@@ -31,6 +35,8 @@ if (employee.performanceScore>80) {
     console.log("Promotion Eligible=False")
 };//adding promotion eligibility
 console.log("Update 2 Employee:",employee)
+
+
 //Task 4:Customer Feedback Records Scenario
 let feedback=[
     {customerName:"Nancy Drew",feedbackText:"Wonderful product.",rating:9},
@@ -40,6 +46,8 @@ let feedback=[
 console.log("Feedback:",feedback)
 feedback.push({customerName:"Ned Nickerson",feedbackText:"Broke easily",rating:2});//adding new customer feedback
 console.log("Updated Feedback:",feedback)
+
+
 //Task 5:Inventory Management System Scenario
 let Inventory={itemName:"Tennis Bracelet",stockCount:233,price:100}
 console.log("Inventory:",Inventory)
